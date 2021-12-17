f90af202ad8f47248ca3ec7aa0152741a1b4bdb1
DJmoster <Zapotichnyi12@gmail.com>
2021-12-17 22:06:01 +0200

    ESP Software V2
    Зроблено:
    
    - Реворк коду
    - Підтримку роботи 2 ядер
    - Переключення криптовалют без затримки
    - Кеш криптовалют для моментального оновлення на екрані
    - Перероблена система відправки реквестів

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,17 +1,7 @@
 
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
-
-#include "DHT.h"
-
-#include "WiFi.h"
-#include <HTTPClient.h>
-#include <ArduinoJson.h>
-
-#include <TimeLib.h>
-
-
 #define DHT_PIN 5
+
+#define BTN_PIN 25
 
 #define WIFI_SSID "TP-Link_Guest"
 #define WIFI_PASSWORD ""
@@ -22,9 +12,27 @@
 #define TIME_API_URL "http://worldtimeapi.org/api/timezone/Europe/Kiev"
 #define CRYPTO_API_URL "https://api.coincap.io/v2/assets/"
 
+#define MAX_CRYPTO_CURRENCIES 4
+
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+#include "DHT.h"
+
+#include "WiFi.h"
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+#include <TimeLib.h>
+#include "GyverTimer.h"
+
+#include "GyverButton.h"
+
 /// Іконки
 byte celsius[] = {B01000, B10100, B01000, B00110, B01000, B01000, B01000, B00110};
 byte connection[] = {B00000, B00001, B00001, B00101, B00101, B10101, B10101, B10101};
+byte connectionNone[] = {B00000, B00000, B00000, B00000, B00000, B00000, B10101, B10101};
 
 /// Дисплей
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -32,144 +40,59 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 /// Датчик температури
 DHT dht(DHT_PIN, DHT11);
 
-/// Json обєкт для отримання результатів реквестів
-DynamicJsonDocument jsonResponse(1024);
+/// Кнопка
+GButton button(BTN_PIN, HIGH_PULL);
 
-/// Змінні наслаштування
+/// Таймер
+GTimer updateTimer(MS, 10000); // Оновлення криптовалюти та температури на дисплеї
+
+
+/* Змінні наслаштування */
 boolean is12HourFormat = true;
 
+short nowCryptoCurrency  = 0;
+short lastCryptoCurrency = nowCryptoCurrency;
 
-void setup(){
-  Serial.begin(115200);
+String cryptoCurrencies[MAX_CRYPTO_CURRENCIES] = {"bitcoin", "cardano"};
+String cryptoCurrenciesCache[MAX_CRYPTO_CURRENCIES];
 
-  /// Ініціалізація датвчика температури
-  dht.begin(); 
-
-  /// Ініціалізація дисплею
-  lcd.init();  
-  lcd.backlight();
-
-  /// Ініціалізація іконок
-  lcd.createChar(0, celsius);    // Градуси цельсія
-  lcd.createChar(1, connection); // Підключено до програми
-
-  lcd.setCursor(2,0);
-  lcd.print("Stera Watch");
-
-  lcd.setCursor(3,1);
-  lcd.print("Starting..");
-  
-  /// Ініціалізація вай фай
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); 
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
-  }
-
-  Serial.println("Connected to the WiFi network");
-
-  /// Ініціалізація часу
-  httpGETRequest(TIME_API_URL);                                 // Отримуємо теперішній час
-  setTime(jsonResponse["unixtime"].as<long>() + (2 * 60 * 60)); // Записуємо теперішній час + 2 години
-
-  /// Отримання ціни криптовалюти
-  String cryptoResult = getNowCrypto("solana");
-  int cryptoPos;
-
-  while (cryptoResult == "0") {
-    if (cryptoResult != "0") {
-      break;
-    }
-    cryptoResult = getNowCrypto("solana");
-  }
-  cryptoPos = (16 - cryptoResult.length()) / 2;
-  
-  /// Отримання температури
-  int temperature;
-  
-  if (!isnan(dht.readTemperature())) {
-    temperature = (int)round(dht.readTemperature());
-  }
-
-  delay(500);
-
-  lcd.clear();
-
-  /// Встановлення криптовалюти на екран
-  lcd.setCursor(cryptoPos, 1);
-  lcd.print(cryptoResult);
-  
-  /// Встановлення температури на екран
-  lcd.setCursor(0,0);
-  lcd.print(temperature);
-
-  /// Встановлення іконок на екран  
-  lcd.setCursor(2,0);
-  lcd.write(0);
-
-  lcd.setCursor(15,0);
-  lcd.write(1);
-}
-
-void loop() { 
-  setNowTime();
-
-  String cryptoResult = getNowCrypto("solana");
-
-  if (cryptoResult != "0") {
-    int position = (16 - cryptoResult.length()) / 2;
-
-    lcd.setCursor(0, 1);
-    lcd.print("                ");
-
-    lcd.setCursor(position, 1);
-    lcd.print(cryptoResult);
-  } 
-  
-  if (!isnan(dht.readTemperature())) {
-    lcd.setCursor(2,0);
-    lcd.write(0);
-    
-    lcd.setCursor(0,0);
-    lcd.print((int)round(dht.readTemperature()));
-  }
-
-  delay(1000);
-}
 
 /// Відправити реквест
-bool httpGETRequest(String serverName) {
-  if ((WiFi.status() == WL_CONNECTED)) {
+StaticJsonDocument<1000> httpGETRequest(String serverName) {
 
-    HTTPClient client;
+  StaticJsonDocument<1000> jsonResponse; // Json документ для отримання результатів реквестів
+
+  HTTPClient client;
+
+  if ((WiFi.status() == WL_CONNECTED)) {
     client.begin(serverName);
 
-    if (client.GET() > 0) {
-      if (client.GET() != 429) {
-        deserializeJson(jsonResponse, client.getStream());
+    short responseCode = client.GET();
 
-        client.end();
-        return true;
-      }
-      else {
+    if (responseCode > 0) {
+      switch (responseCode) {
+
+      case 429:
         Serial.println("Too many requests");
-        
-        client.end();
-        return false;
-      }
+      break;
       
+      default:
+        deserializeJson(jsonResponse, client.getStream());
+      break;
+
+      }
     } 
     else {
       Serial.println("Error on HTTP request");
-      client.end();
     }
+
   } 
   else {
     Serial.println("Connection lost");
   }
-  
-  return false;
+
+  client.end();
+  return jsonResponse;
 }
 
 /// Встановлюємо теперішній час на екран
@@ -199,7 +122,8 @@ void setNowTime() {
     else {
       nowTime = (String)nowHour + nowTime;
     } 
-  } else {
+  } 
+  else {
     if (nowHour < 10) {
       nowTime = "0" + (String)nowHour + nowTime;
     } 
@@ -207,20 +131,21 @@ void setNowTime() {
       nowTime = (String)nowHour + nowTime;
     }
   }
-  
+
   lcd.setCursor(6,0);
   lcd.print(nowTime);
 }
 
 /// Встановлюємо валюту на екран
 String getNowCrypto(String name) {
+  StaticJsonDocument<1000> response = httpGETRequest(CRYPTO_API_URL + name);
   
-  if (httpGETRequest(CRYPTO_API_URL + name)) {
+  if (!response.isNull()) {
     
     String result = ":";
 
-    result      = jsonResponse["data"]["symbol"].as<String>() + result;
-    float price = jsonResponse["data"]["priceUsd"].as<float>();
+    result       = response["data"]["symbol"].as<String>() + result;
+    float price  = response["data"]["priceUsd"].as<float>();
 
     if (price > 1000) {
       result = result + " " + (int)round(price);
@@ -230,11 +155,181 @@ String getNowCrypto(String name) {
     }
 
     Serial.println(result);
+    
+    response.clear();
     return result;
 
   } 
   else {
     Serial.println("Error to get crypto price");
-    return "0";
+
+    response.clear();
+    return "";
+  }
+}
+
+void setCryptoOnDisplay(String cryptoResult) {
+  cryptoCurrenciesCache[nowCryptoCurrency] = cryptoResult; // Запис результату в кеш
+
+  short position = (16 - cryptoResult.length()) / 2;
+
+  lcd.setCursor(0, 1);
+  lcd.print("                ");
+
+  lcd.setCursor(position, 1);
+  lcd.print(cryptoResult);
+}
+
+void swapNowCrypto() {
+  nowCryptoCurrency++;
+
+  if (nowCryptoCurrency != MAX_CRYPTO_CURRENCIES - 1) {
+
+    while (cryptoCurrencies[nowCryptoCurrency] == "") {
+      nowCryptoCurrency++;
+
+      if (nowCryptoCurrency == MAX_CRYPTO_CURRENCIES - 1) nowCryptoCurrency = 0;
+    }
+
+  } else {
+    nowCryptoCurrency = 0;
+  }
+}
+
+
+TaskHandle_t updateTask; // Змінна якою можна керувати 2 ядром
+
+/// Функція яка виконується в 2 ядрі
+void updateButtonTask(void * pvParameters){
+  for(;;) {
+    button.tick();
+
+    if (button.isClick()) swapNowCrypto();
+    delay(10);
+  }
+}
+
+
+void setup(){
+  Serial.begin(115200);
+
+  /// Ініціалізація датвчика температури
+  dht.begin(); 
+
+  /// Ініціалізація дисплею
+  lcd.init();  
+  lcd.backlight();
+
+  /// Ініціалізація іконок
+  lcd.createChar(0, celsius);    // Градуси цельсія
+  lcd.createChar(1, connection); // Підключено до програми
+  lcd.createChar(2, connectionNone); // Не підключено до програми
+
+  /// Меню завантаження годинника
+  lcd.setCursor(2,0);
+  lcd.print("Stera Watch");
+
+  lcd.setCursor(3,1);
+  lcd.print("Starting..");
+  
+  /// Ініціалізація вай фай
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); 
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println("Connected to the WiFi network");
+
+  /// Ініціалізація часу
+  StaticJsonDocument<1000> timeResponse = httpGETRequest(TIME_API_URL); // Отримуємо теперішній час
+
+  if (!timeResponse.isNull()) {
+    setTime(timeResponse["unixtime"].as<long>() + (2 * 60 * 60)); // Записуємо теперішній час + 2 години
+  }
+  timeResponse.clear();
+
+  /// Отримання ціни криптовалюти
+  String cryptoResult = getNowCrypto(cryptoCurrencies[nowCryptoCurrency]);
+
+  while (cryptoResult == "") {
+    if (cryptoResult != "") {
+      break;
+    }
+    cryptoResult = getNowCrypto(cryptoCurrencies[nowCryptoCurrency]);
+  }
+  
+  /// Отримання температури
+  int temperature;
+  
+  if (!isnan(dht.readTemperature())) {
+    temperature = (int)round(dht.readTemperature());
+  }
+
+  /* Ініціалізація 2 ядра */
+  xTaskCreatePinnedToCore(
+                    updateButtonTask,
+                    "updateButton",
+                    1024,
+                    NULL,
+                    1,
+                    &updateTask,
+                    0);
+
+  delay(500);
+
+  lcd.clear();
+
+  /// Встановлення криптовалюти на екран
+  setCryptoOnDisplay(cryptoResult);
+  
+  /// Встановлення температури на екран
+  lcd.setCursor(0,0);
+  lcd.print(temperature);
+
+  /// Встановлення іконок на екран  
+  lcd.setCursor(2,0);
+  lcd.write(0);
+
+  lcd.setCursor(15,0);
+  lcd.write(2);
+}
+
+void loop() {
+  setNowTime();
+
+  if (lastCryptoCurrency != nowCryptoCurrency) {
+    lastCryptoCurrency = nowCryptoCurrency;
+
+    if (cryptoCurrenciesCache[nowCryptoCurrency] != "") {
+      setCryptoOnDisplay(cryptoCurrenciesCache[nowCryptoCurrency]);
+    } 
+    else {
+      lcd.setCursor(0,1);
+      lcd.print("                ");
+    }
+  }
+
+  /* Оновлення криптовалюти кожні 10с */
+  if (updateTimer.isReady()) {
+    Serial.print(ESP.getFreeHeap());
+    
+    /// Відобразити теперішню криптовалюту
+    short momentCryptoCurrency = nowCryptoCurrency;
+    String cryptoResult = getNowCrypto(cryptoCurrencies[momentCryptoCurrency]);
+
+    if (cryptoResult != "" && momentCryptoCurrency == nowCryptoCurrency) {
+      setCryptoOnDisplay(cryptoResult);
+    } 
+    
+    /// Відобразити теперішню температуру
+    if (!isnan(dht.readTemperature())) {
+      lcd.setCursor(2,0);
+      lcd.write(0);
+      
+      lcd.setCursor(0,0);
+      lcd.print((int)round(dht.readTemperature()));
+    }
   }
 }
